8d414b2789a7b994c35b2803a36bb60e268dc670
analauralobos <anhalaurha@gmail.com>
2023-10-06 01:18:33 -0300

    estilos al home

--- src/main/java/com/mycompany/proyectofika/MenuItem.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.proyectofika;
-
-public class MenuItem {
-    private String nombre;
-    private String descripcion;
-    private double precio;
-
-    // Constructor, getters y setters
-}
